a5f4a0089f033fce5205baa7c98649c6843309ae
JackieKirimi <jylnkagendo60@gmail.com>
2025-12-02 23:44:42 +0300

    second commit

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .import views
-#import views
+from . import views
+
 urlpatterns = [
-    path('fruits',views.fruitGarden, name='fruitGarden'),
-    path('home/', views.home, name='home'),   
-    path('create/', views.createFruit, name='create'),
+    path('garden/', views.fruitGarden, name='fruitGarden'),
+
+    path('', views.fetchAllFruits, name='fetchAll'),  # Home page
+    path('home/', views.fetchAllFruits, name='home'),  # Optional alias
+
+    path('create/', views.createFruit, name='create-fruit'),
     
-    path('fruits',views.fetchAllFruits,name='fetchAll'),
     path('fruit/<int:id>/', views.fruitDetail, name='fruit-detail'),
     path('fruit/<int:id>/update/', views.updateFruit, name='update-fruit'),
     path('fruit/<int:id>/delete/', views.deleteFruit, name='delete-fruit'),
